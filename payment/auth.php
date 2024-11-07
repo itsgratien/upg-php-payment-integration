598ee32d5af279fe6c019c7ee096dd8c378bfa1b
gratientuyishimire <gracian2020@gmail.com>
2024-11-07 18:28:55 +0200

    implemented payment auth

--- payment/auth.php
+++ payment/auth.php
@@ -1,12 +1,12 @@
 <?php 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once realpath(__DIR__ . '/../vendor/autoload.php');
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
 header('Content-Type : application/json');
 
 // Load the .env file
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
 $request_method = $_SERVER['REQUEST_METHOD'];
@@ -45,7 +45,7 @@ if($request_method == 'POST'){
     }
 
     // json web token
-    $jwt_payload = ['user_name'=> $username];
+    $jwt_payload = ['iat' => time(),'exp' => time() + 3600,'sub' => 123456,'user_name'=> $username];
 
     $secretKey = $_ENV['UPG_AUTH_SECRET_KEY'];
 
